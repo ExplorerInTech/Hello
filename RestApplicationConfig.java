4f6872a6a7604d259c6e198ba8f7c8b40d8197d6
ExplorerInTech <49986932+ExplorerInTech@users.noreply.github.com>
2019-05-14 14:36:08 -0400

    Delete RestApplicationConfig.java

--- RestApplicationConfig.java
+++
@@ -1,8 +0,0 @@
-package us.mi.lansing.hub;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/rest")
-public class RestApplicationConfig extends Application {
-}
